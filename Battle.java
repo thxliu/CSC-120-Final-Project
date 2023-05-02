e265b0ba1fe5d7cfbecc014e2d5ba7b87ab570c2
Sarah Chavez <scchavez@smith.edu>
2023-05-02 14:05:31 -0400

    trainer

--- Battle.java
+++ Battle.java
@@ -1,13 +1,21 @@
-import java.util.Scanner;
 // import java.time.Duration;
+import java.util.ArrayList;
 
-public class Battle {
+public class Battle extends Trainer {
     int shields;
     Pokemon opponent;
+    int trainer_damage;
+    int opponent_damage;
+    static Trainer myTrainer;
 
     public Battle() {
         this.shields = 2;
     }
+
+    // public void generateOpponent() {
+    //     String[] opponent_list = ["Pikachu", "Charmander"];
+    //     System.out.println("Generating an opponent...");
+    // }
 
     public int defend() {
         this.shields -= 1;
@@ -18,22 +26,30 @@ public class Battle {
     }
 
     public void attack() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println(opponent + " is attacking! \n" + "You have " + this.shields + " left. \n" + "Do you want to use a shield? (Type yes or no)");
-        String shield_use = scanner.nextLine();
+        String shield_use = Input.getScanner().nextLine();
         if (shield_use.equalsIgnoreCase("yes")) {
             defend();
         }
         else {
             System.out.println("Do you wish to counterattack?");
-            String counter_attack = scanner.nextLine();
+            String counter_attack = Input.getScanner().nextLine();
             if (counter_attack.equalsIgnoreCase("yes")) {
-                // use learned attack from trainer class ~ "here are your options"
+                System.out.println("How would you like to attack? Here are your options: ");
+                System.out.println(Trainer.learned_attacks);
                 // if typed in less than 5 seconds, power of attack stays same, if more, attack is less powerful 
         }
 
     }   
-}
+    // public void startBattle() {
+    //     myTrainer.learn();
+    // }
+    }
+    public static void main(String[] args) {
+        Battle myBattle = new Battle();
+        myBattle.learn();
+        myBattle.attack();
+    }
 }
 
 // What user should see
