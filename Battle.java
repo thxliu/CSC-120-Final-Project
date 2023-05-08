bc523c885d10d5c0415cc8a52d10030df2faf3e7
Sarah Chavez <scchavez@smith.edu>
2023-05-08 08:52:56 -0400

    Javadoc + other edits, testing, needs fixing

--- Battle.java
+++ Battle.java
@@ -2,33 +2,57 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Random;
 
-public class Battle extends Trainer {
+/**
+ * Represents a Battle
+ */
+public class Battle {
+  /** The number of shields the Trainer has */
   int shields;
+  /** The Pokemon that is the opponent of the Trainer's Pokemon */
   Pokemon opponent;
+  /** The name of the Pokemon opponent */
   String opponentName;
+  /** The number of experience points (XP) the opponent has */
   private int opponentXP;
-  Trainer trainer;
-  Pokemon pokemon;
+  /** The Trainer that the user is playing as */
+  static Trainer trainer;
+  /** The Trainer's pokemon */
+  // static Pokemon pokemon;
+  /** The name of the Trainer's pokemon*/
   String pokemonName;
+  /** Hashtable listing opponent name and XP */
   static Hashtable<String, Integer> opponent_list;
-  int attack_damage;
+  /** Amount of damage caused by an attack  */
+  int attackDamage;
+  /** The XP of the Trainer's pokemon */
   int pokemonXP;
 
+  /**
+   * Constructor for Battle class.
+   * Uses start() method from Trainer class to create Trainer.
+   * Creates new Pokemon.
+   * Sets Trainer's number of shields = 2.
+   * Initializes list of Pokemon opponents.
+   */
   public Battle() {
-    // trainer = new Trainer();
     Trainer.start();
-    pokemon = new Pokemon();
-    // this.pokemon = new Pokemon();
-    // this.pokemonXP = pokemon.getXp();
+    Trainer.pokemon = new Pokemon();
     this.shields = 2;
     Battle.opponent_list = new Hashtable<String, Integer>();
-    this.attack_damage = 0;
   }
 
+  /**
+   * Accessor for Pokemon opponent's XP.
+   * @return The Pokemon opponent's XP
+   */
   public int getOpponentXP() {
     return this.opponentXP;
   }
 
+  /**
+   * Generates a random Pokemon opponent.
+   * @return The random Pokemon opponent's XP
+   */
   public int generateOpponent() {
     opponent_list = new Hashtable<String, Integer>();
     opponent_list.put("Pikachu", 100);
@@ -42,12 +66,15 @@ public class Battle extends Trainer {
     ArrayList<String> opponentNames = new ArrayList<>(opponent_list.keySet());
     String opponentName = opponentNames.get(randomInt);
     this.opponentName = opponentName;
-    // int opponentXP = opponent_list.get(opponentName);
     this.opponentXP = opponent_list.get(opponentName);
     System.out.println("Generated opponent: " + this.opponentName + " of XP " + this.opponentXP + ".");
     return this.opponentXP;
   }
 
+  /**
+   * Method allowing user to defend using a shield or state they have no shields left.
+   * @return The number of shields the Trainer has
+   */
   public int defend() {
     this.shields -= 1;
     if (this.shields == 0) {
@@ -56,89 +83,96 @@ public class Battle extends Trainer {
     return this.shields;
   }
 
+  /**
+   * Creates battle between Trainer's pokemon and generated opponent.
+   * Checks pokemon and opponent's XP.
+   * Asks user if they want to use a shield use and calls defend().
+   * If a shield is not used, program performs counterattack and times user response, calculating damage accordingly.
+   * Determines winner of Battle.
+   * Asks user if they want to battle again or end the game.
+   */
   public void attack() {
-    // generate opponent
     this.generateOpponent();
-    // while both players are still alive
-    // again
-    while (pokemon.getXp() > 0 && this.opponentXP > 0) {
-      // asks user if they want to use shields
-      System.out.println(this.opponentName + " is attacking! \n" + "You have " + this.shields + " left. \n" +
-          "Do you want to use a shield? (Type yes or no)");
+    while (Trainer.pokemon.getXp() > 0 && this.opponentXP > 0) {
+      System.out.println(this.opponentName + " is attacking! \n" + "You have " + this.shields + " left. \n" + "Do you want to use a shield? (Type yes or no)");
       String shield_use = Input.getScanner().nextLine();
-      // if user wants to use shields, then defend-- xp stays the same
       if (shield_use.equalsIgnoreCase("yes")) {
         defend();
-        if (this.shields >= 0) {
-          throw new RuntimeException("NO MORE SHIELDS LEFT");
+        if (this.shields <= 0) {
+          throw new RuntimeException("NO MORE SHIELDS LEFT. You have lost the battle.");
         }
-        // otherwise, asks if the player wants to counterattack
       } else {
         System.out.println("Do you wish to counterattack? Type yes or no.");
         String counter_attack = Input.getScanner().nextLine();
         if (counter_attack.equalsIgnoreCase("yes")) {
           System.out.println("How would you like to attack? Here are your options: ");
-          // gathers trainer's learned attacks
-          for (int i = 0; i < learned_attacks.size(); i++) {
-            System.out.println(learned_attacks.get(i));
+          for (int i = 0; i < Trainer.learned_attacks.size(); i++) {
+            System.out.println(Trainer.learned_attacks.get(i));
           }
           long startTime = System.currentTimeMillis();
           String chosenAttack = Input.getScanner().nextLine();
-          // times user response to calculate damage
           System.out.println("Please choose an attack. Type of the name of your attack to fight.");
           long endTime = System.currentTimeMillis();
           long time = (endTime - startTime) / 1000;
           System.out.println(time);
           if (time <= 5) {
-            attack_damage = 50;
-            System.out.println("Attacking " + opponentName + " with " + chosenAttack + " with " + attack_damage + "xp");
+            attackDamage = 50;
+            System.out.println("Attacking " + opponentName + " with " + chosenAttack + " with " + attackDamage + "xp");
             opponentXP -= 50;
+            System.out.println(opponentName + " now has " + opponentXP + " XP.");
           } else if (5 < time && time <= 10) {
-            attack_damage = 25;
-            System.out.println("Attacking " + opponentName + " with " + chosenAttack + " with " + attack_damage + "xp");
+            attackDamage = 25;
+            System.out.println("Attacking " + opponentName + " with " + chosenAttack + " with " + attackDamage + "xp");
+            System.out.println(opponentName + " now has " + opponentXP + " XP.");
             opponentXP -= 25;
           } else {
-            attack_damage = 10;
+            attackDamage = 10;
             opponentXP -= 10;
+            System.out.println(opponentName + " now has " + opponentXP + " XP.");
           }
-          System.out.println("Attacking " + opponentName + " with " + chosenAttack + " with " + attack_damage + "xp");
-          // choose not to counterattack
+          System.out.println("Attacking " + opponentName + " with " + chosenAttack + " with " + attackDamage + "xp");
+          System.out.println(opponentName + " now has " + opponentXP + " XP.");
         } else if (counter_attack.equalsIgnoreCase("no")) {
-          System.out.println(
-              "You have chosen not to counterattack. " + opponentName + "'s attack had 50 xp damage.");
+          System.out.println("You have chosen not to counterattack. " + opponentName + "'s attack had 50 xp damage.");
           pokemonXP -= 50;
+          System.out.println("Your pokemon " + pokemonName + " now has " + pokemonXP + " XP.");
+          System.out.println(opponentName + " now has " + opponentXP + " XP.");
         } else {
           throw new RuntimeException("Not a valid option. You must start over.");
         }
       }
     }
-    // if opponent wins
     if (opponentXP > 0) {
-      System.out.println("Sorry, Trainer " + getTrainerName() + ", you have lost the battle. ");
-      System.out.println("Good effort! " + chosenStarter
+      System.out.println("Sorry, Trainer " + Trainer.getTrainerName() + ", you have lost the battle. ");
+      System.out.println("Good effort! " + Pokemon.chosenStarter
           + " is stronger now but better luck next time. Your pokemon has gained 15 XP for their efforts.");
       pokemonXP += 15;
-      // if user wins
     } else {
-      System.out.println("Congratulations, Trainer " + this.name + " you have won the battle!");
+      System.out.println("Congratulations, Trainer " + Trainer.getTrainerName() + " you have won the battle!");
       pokemonXP += 100;
-      System.out.println("As a reward, your pokemon has gained 100 XP! " + " Pokemon " + chosenStarter + " now has "
+      System.out.println("As a reward, your pokemon has gained 100 XP! " + " Pokemon " + Pokemon.chosenStarter + " now has "
           + pokemonXP + " XP.");
     }
-    // asks user if they want to battle again
     System.out.println("Would you like to battle again? Type Y for yes or N for no.");
     String another_battle = Input.getScanner().nextLine();
     if (another_battle.equalsIgnoreCase("Y")) {
       attack();
     } else {
-      System.out.println("Thank you " + trainer.getTrainerName() + "for playing!");
+      System.out.println("Thank you " + Trainer.getTrainerName() + "for playing!");
     }
   }
 
+  /**
+   * Starts battle by calling attack() method.
+   */
   public void startBattle() {
     attack();
   }
 
+  /**
+   * Main method used to test methods in Battle class
+   * @param args The command line arguments
+   */
   public static void main(String[] args) {
     Battle myBattle = new Battle();
     myBattle.startBattle();
--- Trainer.java
+++ Trainer.java
@@ -1,32 +1,63 @@
 import java.util.ArrayList;
 
+/**
+ * Represents a Trainer class
+ */
 public class Trainer extends Pokemon {
 
-  protected String name;
+  /** Trainer's name inputted by the user */
+  private static String name;
+  /** Trainer's trainer_level */
   private int trainer_level;
-  protected static ArrayList<String> learned_attacks = new ArrayList<>();
+  /** static ArrayList of attacks the Trainer has learned */
+  static ArrayList<String> learned_attacks = new ArrayList<>();
+  /** static instance of Pokemon */
   static Pokemon pokemon;
+  /** The pokemon that belongs to the Trainer's XP */
+  static int pokemonXP;
 
+  /**
+   * Constructor for Trainer.
+   * Sets name equal to user input and trainer level equal to 1.
+   * Initializes learned_attacks ArrayList.
+   */
   public Trainer() {
-    System.out.println("Welcome new trainer! Please enter your Trainer name: ");
-    this.name = Input.getScanner().nextLine();
+    System.out.println("Welcome new trainer!");
+    System.out.println("Now, please enter your Trainer name: ");
+    name = Input.getScanner().nextLine();
     System.out.println("Hope you enjoy, " + name + "!");
     this.trainer_level = 1;
     learned_attacks = new ArrayList<>();
   }
 
-  public String getTrainerName() {
-    return this.name;
+  /**
+   * Accessor for Trainer's name.
+   * @return The Trainer's name
+   */
+  public static String getTrainerName() {
+    return Trainer.name;
   }
 
+  /**
+   * Accessor for Trainer's level.
+   * @return The Trainer's level.
+   */
   public int getTrainerLevel() {
     return this.trainer_level;
   }
 
+  /**
+   * Accessor for the Pokemon's XP that belongs to the Trainer.
+   * @return The Pokemon's XP
+   */
   public int getPokemonXp() {
     return pokemon.getXp();
   }
 
+  /**
+   * Allows user to learn new attacks by typing in the name of the attack.
+   * User's pokemon earns 50 XP if all attacks are learned.
+   */
   public static void learn() {
     ArrayList<String> full_attack_list = new ArrayList<>();
     full_attack_list.add("punch");
@@ -45,14 +76,18 @@ public class Trainer extends Pokemon {
       } else if (learn_attack.equalsIgnoreCase("blast")) {
         learned_attacks.add("blast");
       } else {
-        throw new RuntimeException(learn_attack +
+        System.out.println(learn_attack +
             " is not a valid attack. Please enter one of the following: punch, kick, slam, blast.");
+            learn();
       }
       if (learned_attacks.containsAll(full_attack_list)) {
-        int pokemonXP = pokemon.getXp() + 50;
+        try {
+          pokemonXP = pokemon.getXp() + 50;
+        } catch (NullPointerException e) {
+          pokemonXP = pokemon.getXp();
+        }
         System.out.println(
-            "You have unlocked all possible attacks! Your pokemon has earned 50 XP. Your pokemon's XP is now" +
-                pokemonXP);
+            "You have unlocked all possible attacks! Your pokemon has earned 50 XP. Your pokemon's XP is now" + pokemonXP);
         break;
       }
       System.out.println("Do you want to continue learning attacks? Type Y for yes or N for no.");
@@ -60,7 +95,6 @@ public class Trainer extends Pokemon {
       if (userStopLearning.equalsIgnoreCase("Y")) {
         continue;
       } else if (userStopLearning.equalsIgnoreCase("N")) {
-        System.out.println("Come back later to learn more attacks, have a great day!");
         break;
       } else {
         throw new RuntimeException("That was not a valid option. Please try again.");
@@ -72,6 +106,9 @@ public class Trainer extends Pokemon {
     }
   }
 
+  /**
+   * Creates pokemon, calls chooseStarterType, asks if Trainer wants to learn new attacks.
+   */
   public static void start() {
     // Trainer trainer = new Trainer();
     // System.out.println("Welcome new trainer! Please enter your Trainer name: ");
@@ -83,16 +120,21 @@ public class Trainer extends Pokemon {
     System.out.println("Would you like to learn new attacks? Enter Y for yes or N for no.");
     String user_learns = Input.getScanner().nextLine();
     if (user_learns.equalsIgnoreCase("Y")) {
-      learn();
+      Trainer.learn();
     } else if (user_learns.equalsIgnoreCase("N")) {
       System.out.println("Come back later, have a great day!");
     } else {
-      throw new RuntimeException("That was not a valid option. Please try again.");
+      System.out.println("That was not a valid option. Please try again.");
+      Trainer.learn();
     }
   }
 
+  /**
+   * Main method used to test Trainer methods.
+   * @param args The command line args
+   */
   public static void main(String[] args) {
-    // Trainer myTrainer = new Trainer();
-    start();
+    Trainer myTrainer = new Trainer();
+    myTrainer.start();
   }
 }
